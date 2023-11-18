d3a66a138721833e3d7df49f3a4361413e240600
ride-space <qp.vb.1299@gmail.com>
2023-11-18 22:57:54 +0900

    💄 Loadingの位置修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,8 @@ import { Spinner } from '@nextui-org/spinner';
 
 export default function Loading() {
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <Spinner label='Loading...' color='warning' />
+    <div className='flex h-screen w-full items-center justify-center'>
+      <Spinner label='Loading...' color='primary' />
     </div>
   );
 }
