4ce704a4793a0c54e40f36d86613c9bd08ae13c7
ride-space <qp.vb.1299@gmail.com>
2024-07-14 16:07:02 +0900

    不要なコンポーネントを削除

--- src/components/layouts/SidebarLayout.tsx
+++ src/components/layouts/SidebarLayout.tsx
@@ -1,13 +1,8 @@
 'use client';
-import { Card, CardFooter, CardHeader } from '@nextui-org/card';
+import { Card } from '@nextui-org/card';
 import { ScrollShadow } from '@nextui-org/react';
-import { User } from '@nextui-org/user';
-import Link from 'next/link';
 import { useEffect } from 'react';
-import { FaSquareXTwitter } from 'react-icons/fa6';
 import tocbot from 'tocbot';
-
-import { x } from '@/const/urls';
 
 export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
@@ -35,30 +30,6 @@ export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
 export const Sidebar = () => {
   return (
     <aside className='w-[calc(30%_-_40px)]'>
-      <Card>
-        <CardHeader className='justify-between'>
-          <User
-            name='しの'
-            avatarProps={{
-              src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
-            }}
-          />
-        </CardHeader>
-        <div className='px-3 py-0 text-small text-default-400'>
-          <p>
-            都内でフロントエンドエンジニアやってます。アメ車好きの犬派です。
-          </p>
-        </div>
-        <CardFooter className='gap-3'>
-          <Link
-            href={x}
-            target='blank'
-            rel='noopener noreferrer'
-            className='pt-2'>
-            follow me <FaSquareXTwitter size={20} className='inline' />
-          </Link>
-        </CardFooter>
-      </Card>
       <Card className='sticky top-20 mt-5 '>
         <ScrollShadow id='tocNav' className=' px-4 py-5'></ScrollShadow>
       </Card>
